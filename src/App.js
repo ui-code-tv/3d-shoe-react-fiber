5630b4e685773cb1e2620be4d705b13f8427fb8f
ui-cod-tv <uicodetv@googlemail.com>
2022-02-24 15:26:02 +0000

    First rev

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import './index.css';
+import {Suspense, useRef,useState} from 'react'
+import { Canvas} from '@react-three/fiber'
+import {OrbitControls, useGLTF} from '@react-three/drei'
+
+function Model({ ...props }) {
+  const group = useRef()
+  const { nodes, materials } = useGLTF('/shoe.gltf')
+  return (
+    <group ref={group} {...props} dispose={null} scale={3}>
+      <mesh geometry={nodes.shoe.geometry} material={materials.laces} material-color={props.customColors.setStripes}/>
+      <mesh geometry={nodes.shoe_1.geometry} material={materials.mesh} material-color={props.customColors.mesh}/>
+      <mesh geometry={nodes.shoe_2.geometry} material={materials.caps} material-color={props.customColors.soul}/>
+      <mesh geometry={nodes.shoe_3.geometry} material={materials.inner}material-color={props.customColors.soul} />
+      <mesh geometry={nodes.shoe_4.geometry} material={materials.sole} material-color={props.customColors.soul}/>
+      <mesh geometry={nodes.shoe_5.geometry} material={materials.stripes}material-color={props.customColors.stripes} />
+      <mesh geometry={nodes.shoe_6.geometry} material={materials.band} material-color={props.customColors.stripes}/>
+      <mesh geometry={nodes.shoe_7.geometry} material={materials.patch} material-color={props.customColors.soul}/>
+    </group>
+  )
+}
+
 
 function App() {
+ 
+  const [mesh,setMesh] = useState("#ffffff")
+  const [stripes,setStripes] = useState("#ffffff")
+  const [soul,setSoul] = useState("#ffffff")
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="wrapper">
+            <div className="card">
+                <div className="product-canvas">
+                   <Canvas>
+                      <Suspense fallback={null}>
+                          <ambientLight />
+                          <spotLight intensity={0.9} 
+                                     angle={0.1} 
+                                     penumbra={1} 
+                                     position={[10,15,10]}
+                                     castShadow />
+                          <Model customColors={{mesh:mesh, stripes:stripes , soul:soul }}/>
+                          <OrbitControls enablePan={true}
+                                         enableZoom={true}
+                                         enableRotate={true}/>
+                      </Suspense>
+                   </Canvas>
+                </div>
+                <h2>Color chooser</h2>
+                <div className='colors'>
+                    <div>
+                        <input type="color" id="mesh" name="mesh"
+                              value={mesh} 
+                              onChange={(e) => setMesh(e.target.value)}/>
+                        <label for="mesh">Main</label>
+                      </div>
+
+                    <div>
+                        <input type="color" id="stripes" name="stripes"
+                                value= {stripes}
+                                onChange={(e) => setStripes(e.target.value)}/>
+                        <label for="stripes">Stripes</label>
+                    </div>
+                    <div>
+                        <input type="color" id="soul" name="soul"
+                                value={soul} 
+                                onChange={(e) => setSoul(e.target.value)}/>
+                        <label for="soul">Soul</label>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
   );
 }
